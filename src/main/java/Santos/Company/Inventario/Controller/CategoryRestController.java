b430d98638a4316b55afdbdf9b826d4f3212dab9
Santos <santoscarhuachin13@gmail.com>
2025-06-27 00:21:22 -0500

    Servicio cors completado

--- src/main/java/Santos/Company/Inventario/Controller/CategoryRestController.java
+++ src/main/java/Santos/Company/Inventario/Controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package Santos.Company.Inventario.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import Santos.Company.Inventario.Services.ICategoryService;
 
 
 
-
+@CrossOrigin(origins ={"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
